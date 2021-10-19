7cbeca6c05886ec36c6ef703a05225adfe39c6f4
gabrielchiba <gabriel.chiba@hotmail.com>
2021-10-19 12:36:05 -0400

    Add services

---
+++ limits-service/src/main/java/com/gabrielchiba/microservices/limits/controller/LimitsController.java
@@ -0,0 +1,19 @@
+package com.gabrielchiba.microservices.limits.controller;
+
+import com.gabrielchiba.microservices.limits.bean.Limits;
+import com.gabrielchiba.microservices.limits.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+    }
+}
