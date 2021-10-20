5b916d233ed5488839decde9cf25e6c7450eb409
gabrielchiba <gabriel.chiba@hotmail.com>
2021-10-20 16:39:27 -0400

    Add eureka

---
+++ naming-service/src/test/java/com/gabrielchiba/microservices/namingservice/NamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielchiba.microservices.namingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
