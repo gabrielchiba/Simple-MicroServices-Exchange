713c25def8472f4ab72d79689a288f89184a9dad
gabrielchiba <gabriel.chiba@hotmail.com>
2021-10-20 08:26:15 -0400

    Set entities

---
+++ exchange-service/src/main/java/com/gabrielchiba/microservices/exchangeservice/controller/CurrencyExchangeController.java
@@ -0,0 +1,32 @@
+package com.gabrielchiba.microservices.exchangeservice.controller;
+
+import com.gabrielchiba.microservices.exchangeservice.entity.CurrencyExchange;
+//import com.gabrielchiba.microservices.exchangeservice.repository.CurrencyExchangeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+@RestController
+public class CurrencyExchangeController {
+
+//    @Autowired
+//    private CurrencyExchangeRepository repository;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(
+            @PathVariable String from,
+            @PathVariable String to) {
+//        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+
+//        if (currencyExchange == null) {
+//            throw new RuntimeException("Cant find data for " + from + " to " + to);
+//        }
+//        return currencyExchange;
+        return new CurrencyExchange(1000L, from, to,
+        BigDecimal.valueOf(50));
+    }
+}
---
+++ exchange-service/src/main/java/com/gabrielchiba/microservices/exchangeservice/repository/CurrencyExchangeRepository.java
@@ -0,0 +1,8 @@
+//package com.gabrielchiba.microservices.exchangeservice.repository;
+//
+//import com.gabrielchiba.microservices.exchangeservice.entity.CurrencyExchange;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+//    CurrencyExchange findByFromAndTo(String from, String to);
+//}
