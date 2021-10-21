f1331581638677b98444532b36962d1fef63f8d7
gabrielchiba <gabriel.chiba@hotmail.com>
2021-10-21 16:34:43 -0400

    docker suport

--- exchange-service/src/main/java/com/gabrielchiba/microservices/exchangeservice/controller/CurrencyExchangeController.java
+++ exchange-service/src/main/java/com/gabrielchiba/microservices/exchangeservice/controller/CurrencyExchangeController.java
@@ -3,6 +3,8 @@ package com.gabrielchiba.microservices.exchangeservice.controller;
 import com.gabrielchiba.microservices.exchangeservice.entity.CurrencyExchange;
 //import com.gabrielchiba.microservices.exchangeservice.repository.CurrencyExchangeRepository;
 import com.gabrielchiba.microservices.exchangeservice.repository.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,8 @@ import java.util.Currency;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private CurrencyExchangeRepository repository;
 
@@ -21,6 +25,8 @@ public class CurrencyExchangeController {
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to) {
+        logger.info("RetrieveExchangeValue called with {} to {}", from, to);
+
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 
         if (currencyExchange == null) {
